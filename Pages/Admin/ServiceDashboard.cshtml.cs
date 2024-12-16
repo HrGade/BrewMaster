77612667e17bc8408d8dc38f30946c1c85c48878
rhtajik <144002592+rhtajik@users.noreply.github.com>
2024-12-16 12:44:23 +0100

    Design Del

---
+++ Pages/Admin/ServiceDashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BrewMaster.Pages.Admin
+{
+    public class ServiceDashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
